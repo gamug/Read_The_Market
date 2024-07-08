e3631c46b87a8f7d50de9848c7b98b2a96249a70
Munera Gonzalez <g.munera.gonzalez@accenture.com>
2024-07-08 15:54:32 -0500

    Scraping first version

---
+++ src/web_scraping/search.py
@@ -0,0 +1,68 @@
+import datetime, json, time, re, traceback
+from tqdm import tqdm
+from duckduckgo_search import DDGS
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import pandas as pd
+
+companies  = [
+    "Ecopetrol", "Enel", "Grupo Argos", "Terpel", "Vanti", "Promigas", "Bancolombia", "BBVA", "Grupo AVAL", "Davivienda", "Credibanco",
+    "Grupo Sura", "Itau", "Nequi", "Grupo Exito", "Cencosud", "Tiendas D1", "Alkosto", "Falabella", "Sodimac", "Caracol", "Empresa Claro", "ETB",
+    'GeoPark', 'Empresa Brilla', 'Banco de Occidente', 'Cenit'
+    ]
+
+sources = ['larepublica', 'semana', 'eltiempo', 'lasillavacia', 'elcolombiano', 'portafolio', 'elespectador']
+
+sources_links = {
+    'semana': 'semana.com', 'larepublica': 'larepublica.co', 'eltiempo': 'eltiempo.com', 'lasillavacia': 'lasillavacia.com',
+    'elcolombiano': 'elcolombiano.com', 'portafolio': 'portafolio.co', 'elespectador': 'elespectador.com'
+    }
+
+def api_search(company, source, sources_links, sleep):
+    results = DDGS().text(
+                    keywords=f'{company} site:{sources_links[source]}',
+                    max_results=10,
+                    safesearch='off',
+                    timelimit='w'
+                    )
+    time.sleep(3)
+    return results
+
+def selenium_search(driver, company, source, sources_links):
+    driver.get(f'https://duckduckgo.com/?q={company}+site%3A{sources_links[source]}&t=h_&df=w&ia=web')
+    time.sleep(1) # give page a chance to fully load
+    content = driver.page_source
+    links = pd.Series(re.findall(f'(<a href=".*?" rel)', content, flags=re.IGNORECASE))
+    links = links.str.replace('<a href=', '').str.replace('"', '').str.replace(' rel', '').drop_duplicates()
+    links = links[links.str.contains('https\://www.+', regex=True, case=False)]
+    return links
+
+def get_news_links(companies, sources, sources_links):
+    search_results = {}
+    pbar = tqdm(total=len(companies)*len(sources))
+    driver = webdriver.Chrome()
+    try:
+        for source in sources:
+            aux = {}
+            for company in companies:
+                pbar.update(1)
+                pbar.set_description(f'source:{source}-company:{company}')
+                results = selenium_search(driver, company, source, sources_links)
+                if len(results):
+                    if isinstance(results, dict):
+                        aux[company] = [json_['href'] for json_ in results]
+                    else:
+                        aux[company] = results.tolist()
+            if len(aux):
+                search_results[source] = aux
+        pbar.close()
+        driver.quit()
+        week_number = datetime.datetime.now().isocalendar()[1]
+        with open(f'engine_search/week_{week_number}.json', 'w') as f:
+            json.dump(search_results, f)
+        return search_results
+    except Exception:
+        traceback.print_exc()
+        print('failed scraping...')
+        pbar.close()
+        driver.quit()
